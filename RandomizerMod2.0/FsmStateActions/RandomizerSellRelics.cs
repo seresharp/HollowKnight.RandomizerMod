883240787e7bff89c77adb63ae949f018e97e3c1
Seanpr <separile@ucsc.edu>
2018-06-02 14:25:41 -0700

    Added check for defender's crest in sell all

--- RandomizerMod2.0/FsmStateActions/RandomizerSellRelics.cs
+++ RandomizerMod2.0/FsmStateActions/RandomizerSellRelics.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using HutongGames.PlayMaker;
+﻿using HutongGames.PlayMaker;
 
 namespace RandomizerMod.FsmStateActions
 {
@@ -10,25 +6,28 @@ namespace RandomizerMod.FsmStateActions
     {
         public override void OnEnter()
         {
-            int money = PlayerData.instance.trinket1 * 200;
-            money += PlayerData.instance.trinket2 * 450;
-            money += PlayerData.instance.trinket3 * 800;
-            money += PlayerData.instance.trinket4 * 1200;
-
-            if (money > 0)
+            if (!PlayerData.instance.GetBool("equippedCharm_10"))
             {
-                HeroController.instance.AddGeo(money);
+                int money = PlayerData.instance.trinket1 * 200;
+                money += PlayerData.instance.trinket2 * 450;
+                money += PlayerData.instance.trinket3 * 800;
+                money += PlayerData.instance.trinket4 * 1200;
+
+                if (money > 0)
+                {
+                    HeroController.instance.AddGeo(money);
+                }
+
+                PlayerData.instance.soldTrinket1 += PlayerData.instance.trinket1;
+                PlayerData.instance.soldTrinket2 += PlayerData.instance.trinket2;
+                PlayerData.instance.soldTrinket3 += PlayerData.instance.trinket3;
+                PlayerData.instance.soldTrinket4 += PlayerData.instance.trinket4;
+
+                PlayerData.instance.trinket1 = 0;
+                PlayerData.instance.trinket2 = 0;
+                PlayerData.instance.trinket3 = 0;
+                PlayerData.instance.trinket4 = 0;
             }
-
-            PlayerData.instance.soldTrinket1 += PlayerData.instance.trinket1;
-            PlayerData.instance.soldTrinket2 += PlayerData.instance.trinket2;
-            PlayerData.instance.soldTrinket3 += PlayerData.instance.trinket3;
-            PlayerData.instance.soldTrinket4 += PlayerData.instance.trinket4;
-
-            PlayerData.instance.trinket1 = 0;
-            PlayerData.instance.trinket2 = 0;
-            PlayerData.instance.trinket3 = 0;
-            PlayerData.instance.trinket4 = 0;
 
             Finish();
         }
